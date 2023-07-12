86db8b75b3bbf2bc3a10d8459cb0d7a0e40b90cb
Abhishek Poddar <67195536+abhi8054@users.noreply.github.com>
2023-07-13 01:53:24 +0530

    done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { useState } from 'react';
 import './App.css';
 import { db } from './config/firebase';
 import { addDoc, collection } from 'firebase/firestore';
-
+import { ToastContainer, toast } from 'react-toastify';
+import Loading from './components/Loading';
 function App() {
   const [data,setData] = useState({
     name:'',
@@ -10,7 +11,7 @@ function App() {
     phone:'',
     description:''
   })
-
+  const [loading,setLoading] = useState(false)
   const collectionRef = collection(db,'users')
 
   const inputHandler = (e) =>{
@@ -22,56 +23,103 @@ function App() {
 
 
   const submitHandler = (e) =>{
-    console.log("Abhishek");
- 
+      e.preventDefault()
+      setLoading(true)
       addDoc(collectionRef,data).then(res =>{
         console.log(res);
-      });
+        setData({
+          name:'',
+          email:'',
+          phone:'',
+          description:''
+        })
+        setLoading(false)
+        toast.success('Response Submitted', {
+          position: "bottom-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          });
+      }).catch((err) =>{
+        setLoading(false)
+        console.log(err);
+        toast.error('Response Submitted', {
+          position: "bottom-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          });
+      })
    
   }
 
   return (
-    <div className="container">
-      <div className='wrapper'>
-        <div className='left'>
-          <div>
-            <h1>Hire Remote <br></br>Igaming Developer</h1>
-          </div>
-          <div>
-            <a href="https://api.whatsapp.com/send/?phone=+441892710894&text&type=phone_number&app_absent=0"><i class="fa-brands fa-whatsapp"></i> whatsapp</a>
-            <i className="fa-solid fa-bell"></i>
-          </div>
-        </div>
-        <div className='right'>
-          <div className='right_wrapper'>
-            <h1>Get a Free Consultation</h1>
-            <div className='desc'>
-              <p>Have a project that we could work together on?</p>
-              <p>We would love to talk</p>
+    <>
+    <form onSubmit={submitHandler}>
+      <div className="container">
+        <div className='wrapper'>
+          <div className='left'>
+            <div>
+              <h1>Hire Remote <br></br>Igaming Developer</h1>
             </div>
-            <div className='input_fields'>
-              <div className='inputs'>
-                <input onChange={inputHandler} type='text' name='name' placeholder='Full Name' required/>
-              </div>
-              <div className='inputs'>
-                <input onChange={inputHandler} type='email' name='email' placeholder='Email' required />
-              </div>
-              <div className='inputs'>
-                <input onChange={inputHandler} type='number' name='phone' placeholder='Phone' required />
-              </div>
-              <div className='text_area'>
-                <textarea placeholder='Enter a brief description of your project' onChange={inputHandler} name='description'>
-                  
-                </textarea>
-              </div>
-              <div className='btn'>
-                <button onClick={submitHandler}>Submit</button>
-              </div>
+            <div>
+              <a href="https://api.whatsapp.com/send/?phone=+441892710894&text&type=phone_number&app_absent=0"><i class="fa-brands fa-whatsapp"></i> whatsapp</a>
+              <i className="fa-solid fa-bell"></i>
+            </div>
           </div>
+          <div className='right'>
+            <div className='right_wrapper'>
+              <h1>Get a Free Consultation</h1>
+              <div className='desc'>
+                <p>Have a project that we could work together on?</p>
+                <p>We would love to talk</p>
+              </div>
+              <div className='input_fields'>
+                <div className='inputs'>
+                  <input onChange={inputHandler} value={data.name} type='text' name='name' placeholder='Full Name' required/>
+                </div>
+                <div className='inputs'>
+                  <input onChange={inputHandler} value={data.email}  type='email' name='email' placeholder='Email' required />
+                </div>
+                <div className='inputs'>
+                  <input onChange={inputHandler} value={data.phone}  type='number' name='phone' placeholder='Phone' required />
+                </div>
+                <div className='text_area'>
+                  <textarea placeholder='Enter a brief description of your project' value={data.description}  onChange={inputHandler} name='description'>
+                    
+                  </textarea>
+                </div>
+                <div className='btn'>
+                  <button disabled={loading} type='submit'>{loading ? <Loading /> : 'Submit'}</button>
+                </div>
+            </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </form>
+
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        />
+    </>
   );
 }
 
